6ee44fb9636910a82d2fa1b7ddf531a5df115384
1mishanya107 <1mishanya107@bk.ru>
2021-10-11 20:13:00 +0400

    Дз9
    дз9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body")
+const btn = document.querySelector(".change-color")
+const text = document.querySelector(".color")
+// console.log(btn)
+
+btn.addEventListener("click", () => {
+  body.style.backgroundColor = `${getRandomHexColor()}`
+  text.textContent = `${getRandomHexColor()}`
+})
