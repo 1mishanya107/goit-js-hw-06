dfa7227d26e291acdc03d601a88476efd27e832e
1mishanya107 <1mishanya107@bk.ru>
2021-10-11 20:01:29 +0400

    дз8
    дз8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector(".login-form")
+form.addEventListener("submit", control);
 
+function control(event) {
+    event.preventDefault()
+    const { elements: { email, password } } = event.currentTarget
+    // console.log()
+
+        
+        if (email.value === "" || password.value === "") {
+    return alert("Внимание! Для отправки формы все поля должны быть заполнены!");
+        }
+    
+    const elem = {
+        email: email.value,
+        password: password.value,
+    }
+    console.log(elem)
+    
+    
+    
+    
+    event.currentTarget.reset();
+}
