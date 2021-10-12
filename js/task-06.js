45f149a4430c73cb179b5f26328dae92281ae521
1mishanya107 <1mishanya107@bk.ru>
2021-10-12 18:47:22 +0400

    переделал задачу6

--- js/task-06.js
+++ js/task-06.js
@@ -5,7 +5,7 @@ const input = document.querySelector('#validation-input')
 
 
 input.addEventListener('blur', () => {
-    if (input.value.length === 6) {
+    if (input.value.length === Number(input.dataset.length)) {
         input.classList.add("valid")
     }
     else input.classList.add("invalid")
